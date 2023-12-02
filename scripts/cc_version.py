cae204eca30ea780c8d8541db13e972dbdadd793
catboxanon <122327233+catboxanon@users.noreply.github.com>
2023-12-02 15:05:42 -0500

    Update cc_version.py

--- scripts/cc_version.py
+++ scripts/cc_version.py
@@ -5,12 +5,12 @@ import json
 VERSION = 'v1.5.0'
 
 def clean_outdated(EXT_NAME:str):
-    with open(scripts.basedir() + '/' + 'ui-config.json', 'r') as json_file:
+    with open(scripts.basedir() + '/' + 'ui-config.json', 'r', encoding='utf8') as json_file:
         configs = json.loads(json_file.read())
 
     cleaned_configs = {key: value for key, value in configs.items() if EXT_NAME not in key}
 
-    with open(scripts.basedir() + '/' + 'ui-config.json', 'w') as json_file:
+    with open(scripts.basedir() + '/' + 'ui-config.json', 'w', encoding='utf8') as json_file:
         json.dump(cleaned_configs, json_file)
 
 def refresh_sliders():
